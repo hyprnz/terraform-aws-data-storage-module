ce36ec65dd18ddc90ae462c851b2963a4aafa5d7
Reuben Dunn <reuben.dunn@hypr.co.nz>
2020-01-15 10:05:02 +1300

    Updated rds example to use default VPC

--- examples/rds/main.tf
+++ examples/rds/main.tf
@@ -1,26 +1,21 @@
-resource "aws_vpc" "db_vpc" {
-  cidr_block = "10.0.0.0/16"
+data "aws_vpcs" "default" {
+  filter {
+    name = "isDefault"
+    values = ["true"]
+  }
 }
 
-resource "aws_subnet" "db_subnet_a" {
-  cidr_block        = "10.0.0.0/24"
-  vpc_id            = "${aws_vpc.db_vpc.id}"
-  availability_zone = "ap-southeast-2a"
-}
-
-resource "aws_subnet" "db_subnet_b" {
-  cidr_block        = "10.0.1.0/24"
-  vpc_id            = "${aws_vpc.db_vpc.id}"
-  availability_zone = "ap-southeast-2b"
+data "aws_subnet_ids" "default" {
+  vpc_id = tolist(data.aws_vpcs.default.ids)[0]
 }
 
 resource "aws_db_subnet_group" "db_subnetgroup" {
-  subnet_ids = ["${aws_subnet.db_subnet_a.id}", "${aws_subnet.db_subnet_b.id}"]
-  name       = "example-group"
+  subnet_ids = data.aws_subnet_ids.default.ids
+  name       = "data-storage-rds-example"
 }
 
 resource "aws_security_group" "db_security_group" {
-  vpc_id = "${aws_vpc.db_vpc.id}"
+  vpc_id = tolist(data.aws_vpcs.default.ids)[0]
 
   ingress {
     from_port   = 5432
@@ -35,41 +30,63 @@ resource "aws_security_group" "db_security_group" {
     to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = map("Name", "data-storage-rds-example-security-group")
 }
 
 module "example_datastore_rds" {
   source = "../../"
 
+  providers = {
+    aws = aws
+  }
 
+  create_rds_instance = true
+  rds_database_name   = "example"
+  rds_identifier      = "example-postgres-dev"
 
-  create_rds_instance    = true
-  rds_database_name      = "test_db_postgress"
-  rds_identifier         = "example-postgres-dev"
-  rds_subnet_group       = "${aws_db_subnet_group.db_subnetgroup.name}"
-  rds_security_group_ids = ["${aws_security_group.db_security_group.id}"]
-  rds_password           = "reallylongpassword"
+  rds_subnet_group       = aws_db_subnet_group.db_subnetgroup.name
+  rds_security_group_ids = [aws_security_group.db_security_group.id]
+
+  rds_password = "reallylongpassword"
 }
 
 provider "aws" {
-  region = "ap-southeast-2"
+  region = var.aws_region
+}
+
+variable "aws_region" {
+  default = "ap-southeast-2"
+}
+
+output "debug_vpc_id" {
+  value = data.aws_vpcs.default.ids
+}
+
+output "debug_subnet_ids" {
+  value = data.aws_subnet_ids.default.ids
+}
+
+output "debug_subgroup_name" {
+  value = aws_db_subnet_group.db_subnetgroup.name
 }
 
 output "endpoint" {
-  value = "${module.example_datastore_rds.rds_instance_endpoint}"
+  value = module.example_datastore_rds.rds_instance_endpoint
 }
 
 output "instance_is" {
-  value = "${module.example_datastore_rds.rds_instance_id}"
+  value = module.example_datastore_rds.rds_instance_id
 }
 
 output "db_name" {
-  value = "${module.example_datastore_rds.rds_db_name}"
+  value = module.example_datastore_rds.rds_db_name
 }
 
 output "db_user" {
-  value = "${module.example_datastore_rds.rds_db_user}"
+  value = module.example_datastore_rds.rds_db_user
 }
 
 output "db_url" {
-  value = "${module.example_datastore_rds.rds_db_url}"
+  value = module.example_datastore_rds.rds_db_url
 }
