ed8a0ebf5c27314a0df919d80bd02893060b0f96
Reuben Dunn <reuben.dunn@hypr.co.nz>
2020-01-14 13:18:37 +1300

    Updated rds example to work with default vpc & subnets

--- examples/rds/main.tf
+++ examples/rds/main.tf
@@ -1,3 +1,38 @@
+data "aws_vpcs" "default" {
+  filter = {
+    name = "isDefault", values = ["true"]
+  }
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = "${data.aws_vpcs.default.ids[0]}"
+}
+
+resource "aws_db_subnet_group" "db_subnetgroup" {
+  subnet_ids = ["${data.aws_subnet_ids.default.ids}"]
+  name       = "data-storage-rds-example"
+}
+
+resource "aws_security_group" "db_security_group" {
+  vpc_id = "${data.aws_vpcs.default.ids[0]}"
+
+  ingress {
+    from_port   = 5432
+    protocol    = "tcp"
+    to_port     = 5432
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = "${map("Name", "data-storage-rds-example-security-group")}"
+}
+
 module "example_datastore_rds" {
   source = "../../"
 
@@ -9,14 +44,30 @@ module "example_datastore_rds" {
   rds_database_name   = "example"
   rds_identifier      = "example-postgres-dev"
 
-  rds_subnet_group       = "example-group"
-  rds_security_group_ids = ["sg-0123456789"]
+  rds_subnet_group       = "${aws_db_subnet_group.db_subnetgroup.name}"
+  rds_security_group_ids = ["${aws_security_group.db_security_group.id}"]
 
   rds_password = "reallylongpassword"
 }
 
 provider "aws" {
-  region = "ap-southeast-2"
+  region = "${var.aws_region}"
+}
+
+variable "aws_region" {
+  default = "ap-southeast-2"
+}
+
+output "debug_vpc_id" {
+  value = "${data.aws_vpcs.default.ids}"
+}
+
+output "debug_subnet_ids" {
+  value = "${data.aws_subnet_ids.default.ids}"
+}
+
+output "debug_subgroup_name" {
+  value = "${aws_db_subnet_group.db_subnetgroup.name}"
 }
 
 output "endpoint" {
