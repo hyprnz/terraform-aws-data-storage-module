c08d38c46fcb6167ee8475c962f72623f7155755
Reuben Dunn <reuben.dunn@hypr.co.nz>
2020-06-10 10:57:22 +1200

    Ported RDS changes from tag 0.1.6-01

--- rds.tf
+++ rds.tf
@@ -14,16 +14,18 @@ resource "aws_db_instance" "this" {
   username = local.username
   password = var.rds_password
 
-  db_subnet_group_name    = var.rds_subnet_group
-  vpc_security_group_ids  = var.rds_security_group_ids
-  allocated_storage       = var.rds_allocated_storage
-  backup_retention_period = var.backup_retention_period
-  iops                    = var.rds_iops
-  monitoring_interval     = var.rds_monitoring_interval
-  backup_window           = var.rds_backup_window
-  skip_final_snapshot     = var.rds_skip_final_snapshot
-  storage_encrypted       = var.rds_storage_encrypted
-  kms_key_id              = var.rds_storage_encryption_kms_key_arn
+  db_subnet_group_name         = var.rds_subnet_group
+  vpc_security_group_ids       = var.rds_security_group_ids
+  allocated_storage            = var.rds_allocated_storage
+  backup_retention_period      = var.backup_retention_period
+  iops                         = var.rds_iops
+  monitoring_interval          = var.rds_monitoring_interval
+  monitoring_role_arn          = var.rds_monitoring_role_arn
+  performance_insights_enabled = var.enable_performance_insights
+  backup_window                = var.rds_backup_window
+  skip_final_snapshot          = var.rds_skip_final_snapshot
+  storage_encrypted            = var.rds_storage_encrypted
+  kms_key_id                   = var.rds_storage_encryption_kms_key_arn
   tags = merge(
     {
       "rds_engine" = var.rds_engine
