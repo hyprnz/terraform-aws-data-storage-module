cb219464f6ad8ebec160c30e44015e0d6f1dbd00
Reuben Dunn <reuben.dunn@hypr.co.nz>
2020-10-14 17:17:19 +1300

    Refactored dynamodb policy name to new standard

--- dynamodb_iam.tf
+++ dynamodb_iam.tf
@@ -2,7 +2,7 @@ data "aws_iam_policy_document" "dynamodb_table_datastore" {
   count = local.create_dynamodb ? 1 : 0
 
   statement {
-    sid = "DynamodbDataStore${replace(title(var.dynamodb_table_name), "/-| |_/", "")}Actions"
+    sid = "DataStorageDynamodbActions"
 
     effect = "Allow"
 
@@ -22,7 +22,7 @@ data "aws_iam_policy_document" "dynamodb_table_datastore" {
   }
 
   statement {
-    sid = "DynamodbDataStore${replace(title(var.dynamodb_table_name), "/-| |_/", "")}StreamActions"
+    sid = "DataStorageDynamodbStreamActions"
 
     effect = "Allow"
 
@@ -41,7 +41,7 @@ data "aws_iam_policy_document" "dynamodb_table_datastore" {
 resource "aws_iam_policy" "dynamodb_table_datastore" {
   count = local.create_dynamodb ? 1 : 0
 
-  name        = "DynamodbDataStoreTableAccess${replace(title(var.dynamodb_table_name), "/-| |_/", "")}Policy"
+  name        = "DataStorage-Dynamodb-${replace(var.dynamodb_table_name, "/-| |_/", "")}Policy"
   policy      = data.aws_iam_policy_document.dynamodb_table_datastore[0].json
   description = "Grants permissions to access the dynamodb table and associated objects"
 }
