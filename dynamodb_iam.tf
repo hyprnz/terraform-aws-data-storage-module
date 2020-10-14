e6d9a9f2394cad619a3921f6a2a2d935908c4cda
Reuben Dunn <reuben.dunn@hypr.co.nz>
2020-10-14 17:40:08 +1300

    Updated dynamobdb policy name to look better ;-)

--- dynamodb_iam.tf
+++ dynamodb_iam.tf
@@ -41,7 +41,7 @@ data "aws_iam_policy_document" "dynamodb_table_datastore" {
 resource "aws_iam_policy" "dynamodb_table_datastore" {
   count = local.create_dynamodb ? 1 : 0
 
-  name        = "DataStorage-Dynamodb-${replace(var.dynamodb_table_name, "/-| |_/", "")}Policy"
+  name        = "DataStorage-Dynamodb-${replace(var.dynamodb_table_name, "/-| |_/", "")}-Policy"
   policy      = data.aws_iam_policy_document.dynamodb_table_datastore[0].json
   description = "Grants permissions to access the dynamodb table and associated objects"
 }
