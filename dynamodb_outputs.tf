5fae394df13b55077800b00a93808ab39ae73023
Reuben Dunn <r.dunn@hypr.nz>
2025-02-26 14:18:51 +1300

    fix(ddb outputs): updated ddb outputs with try function

--- dynamodb_outputs.tf
+++ dynamodb_outputs.tf
@@ -1,16 +1,16 @@
 output "dynamodb_table_name" {
   description = "DynamoDB table name"
-  value       = join(",", aws_dynamodb_table.this[*].name)
+  value       = try(aws_dynamodb_table.this[0].name, "")
 }
 
 output "dynamodb_table_id" {
   description = "DynamoDB table ID"
-  value       = join(",", aws_dynamodb_table.this[*].id)
+  value       = try(aws_dynamodb_table.this[0].id, "")
 }
 
 output "dynamodb_table_arn" {
   description = "DynamoDB table ARN"
-  value       = join(",", aws_dynamodb_table.this[*].arn)
+  value       = try(aws_dynamodb_table.this[0].arn, "")
 }
 
 output "dynamodb_global_secondary_index_names" {
@@ -25,15 +25,15 @@ output "dynamodb_local_secondary_index_names" {
 
 output "dynamodb_table_stream_arn" {
   description = "DynamoDB table stream ARN"
-  value       = join(",", aws_dynamodb_table.this[*].stream_arn)
+  value       = try(aws_dynamodb_table.this[0].stream_arn, "")
 }
 
 output "dynamodb_table_stream_label" {
   description = "DynamoDB table stream label"
-  value       = join(",", aws_dynamodb_table.this[*].stream_label)
+  value       = try(aws_dynamodb_table.this[0].stream_label, "")
 }
 
 output "dynamodb_table_policy_arn" {
   description = "Policy arn to be attached to an execution role defined in the parent module."
-  value       = join("", aws_iam_policy.dynamodb_table_datastore[*].arn)
+  value       = try(aws_iam_policy.dynamodb_table_datastore[0].arn, "")
 }
