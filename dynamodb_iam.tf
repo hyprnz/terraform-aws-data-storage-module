57067851f0c75dc7daba3534b337429431f6266b
Reuben Dunn <r.dunn@hypr.nz>
2025-02-26 14:17:55 +1300

    fix(ddb iam): update ddb stream policy and added options for table and stream iam statements

--- dynamodb_iam.tf
+++ dynamodb_iam.tf
@@ -1,26 +1,51 @@
+locals {
+  count_ddb_table_policy = local.create_dynamodb && !var.dynamodb_enable_streams ? 1 : 0
+
+  count_ddb_table_and_stream_policy = local.create_dynamodb && var.dynamodb_enable_streams ? 1 : 0
+
+  dynamodb_table_iam_actions = [
+    "dynamodb:DescribeTable",
+    "dynamodb:BatchGetItem",
+    "dynamodb:GetItem",
+    "dynamodb:Query",
+    "dynamodb:Scan",
+    "dynamodb:BatchWriteItem",
+    "dynamodb:PutItem",
+    "dynamodb:DeleteItem",
+    "dynamodb:UpdateItem"
+  ]
+}
+
 data "aws_iam_policy_document" "dynamodb_table_datastore" {
-  count = local.count_dynamodb
+  count = local.count_ddb_table_policy
 
   statement {
     sid = "DataStorageDynamodbActions"
 
     effect = "Allow"
 
-    actions = [
-      "dynamodb:DescribeTable",
-      "dynamodb:BatchGetItem",
-      "dynamodb:GetItem",
-      "dynamodb:Query",
-      "dynamodb:Scan",
-      "dynamodb:BatchWriteItem",
-      "dynamodb:PutItem",
-      "dynamodb:DeleteItem",
-      "dynamodb:UpdateItem"
-    ]
+    actions = local.dynamodb_table_iam_actions
 
     resources = [
-      join("", aws_dynamodb_table.this[*].arn),
-      "${join("", aws_dynamodb_table.this[*].arn)}/*"
+      aws_dynamodb_table.this[0].arn,
+      "${aws_dynamodb_table.this[0].arn}/*"
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "dynamodb_table_stream_datastore" {
+  count = local.count_ddb_table_and_stream_policy
+
+  statement {
+    sid = "DataStorageDynamodbActions"
+
+    effect = "Allow"
+
+    actions = local.dynamodb_table_iam_actions
+
+    resources = [
+      aws_dynamodb_table.this[0].arn,
+      "${aws_dynamodb_table.this[0].arn}/*"
     ]
   }
 
@@ -30,11 +55,14 @@ data "aws_iam_policy_document" "dynamodb_table_datastore" {
     effect = "Allow"
 
     actions = [
-      "dynamodb:GetRecords"
+      "dynamodb:DescribeStream",
+      "dynamodb:GetRecords",
+      "dynamodb:GetShardIterator",
+      "dynamodb:ListStreams"
     ]
 
     resources = [
-      "arn:aws:dynamodb:*:*:table/${join("", aws_dynamodb_table.this[*].name)}/stream/*"
+      "${aws_dynamodb_table.this[0].arn}/stream/*"
     ]
   }
 }
@@ -45,7 +73,7 @@ resource "aws_iam_policy" "dynamodb_table_datastore" {
   count = local.count_dynamodb
 
   name        = "${var.dynamodb_table_name}-DS-ddb"
-  policy      = data.aws_iam_policy_document.dynamodb_table_datastore[0].json
+  policy      = local.count_ddb_table_and_stream_policy == 1 ? data.aws_iam_policy_document.dynamodb_table_stream_datastore[0].json : data.aws_iam_policy_document.dynamodb_table_datastore[0].json
   path        = var.iam_resource_path
   description = "Grants permissions to access the dynamodb table and associated objects"
 }
