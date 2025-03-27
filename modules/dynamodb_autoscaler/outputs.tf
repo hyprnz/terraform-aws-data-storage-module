f76e10a23168d8db946c14ebecc64c15bf1cc384
Bing Shi <bing@hypr.nz>
2025-03-27 15:26:44 +1300

    chore(dynamodb): add outputs for dynamodb autoscaler module

--- modules/dynamodb_autoscaler/outputs.tf
+++ modules/dynamodb_autoscaler/outputs.tf
@@ -1,1 +1,15 @@
+output "aws_appautoscaling_target" {
+  description = "The DynamoDB table autoscaling targets"
+  value = {
+    read_target  = aws_appautoscaling_target.read_target
+    write_target = aws_appautoscaling_target.write_target
+  }
+}
 
+output "aws_appautoscaling_policy" {
+  description = "The DynamoDB table autoscaling policies"
+  value = {
+    read_policy  = aws_appautoscaling_policy.read_policy
+    write_policy = aws_appautoscaling_policy.write_policy
+  }
+}
