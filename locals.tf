1a9e760be3e0ab318404af13a819ab312e156bbc
Bing Shi <bing@hypr.nz>
2025-03-27 15:26:24 +1300

    style: format locals.tf for better readability

--- locals.tf
+++ locals.tf
@@ -4,10 +4,10 @@ locals {
 
   create_dynamodb          = var.enable_datastore && var.create_dynamodb_table
   create_dynamodb_insights = var.enable_datastore && var.create_dynamodb_table && var.dynamodb_enable_insights
-  count_dynamodb          = local.create_dynamodb ? 1 : 0
-  count_dynamodb_insights = local.create_dynamodb_insights ? 1 : 0
-  create_rds_instance = var.enable_datastore && var.create_rds_instance
-  count_rds_instance  = local.create_rds_instance ? 1 : 0
+  count_dynamodb           = local.create_dynamodb ? 1 : 0
+  count_dynamodb_insights  = local.create_dynamodb_insights ? 1 : 0
+  create_rds_instance      = var.enable_datastore && var.create_rds_instance
+  count_rds_instance       = local.create_rds_instance ? 1 : 0
 
   create_rds_instance_with_snapshot = var.enable_datastore && var.use_rds_snapshot && (!local.create_rds_instance)
   count_rds_instance_with_snapshot  = local.create_rds_instance_with_snapshot ? 1 : 0
